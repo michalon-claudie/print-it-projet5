732edf61274ccf7239d000891b5cd286815ec3e0
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-08-21 09:44:35 +0200

    "add const on element and create function-dot"

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,11 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+let currentSlide = 0;
+
+const dots = document.querySelectorAll ('.dot');
+const bannerP = document.querySelector('.banner-p');
+const bannerSlide = document.querySelector('.banner-img')
 
 const left = document.querySelector ('.arrow_left');
 console.log(left);
@@ -23,15 +28,23 @@ console.log(left);
 const right = document.querySelector ('.arrow_right');
 console.log(right);
 
-function moveLeft(){
-	alert('Tu as cliqué sur la flèche gauche');
-	console.log('Tu as cliqué sur la flèche gauche');
+/***Functions***/
+
+function activeDots()
+{
+	dots.forEach((dot,index) =>
+	{
+		dot.classList.remove("dot_selected");
+		if (index === currentSlide)
+		{
+			dot.classList.add("dot_selected");
+		}
+	}
+	)
 }
+
+
+/***Event***/
 
 left.addEventListener('click',moveLeft);
-
-function moveRight(){
-	alert('Tu as cliqué sur la flèche droite');
-	console.log('Tu as cliqué sur la flèche droite');
-}
 right.addEventListener('click',moveRight);
